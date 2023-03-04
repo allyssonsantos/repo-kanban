dac01691c230881f6fd5f78741c733b672c14194
Allysson Santos <aallysson0@gmail.com>
2023-03-04 16:44:00 -0300

    chore: change logo

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './Button';
 export * from './Box';
 export * from './Card';
-export * from './CodeSandboxLogo';
 export * from './Flex';
 export * from './Input';
 export * from './ThemeSwitcher';
