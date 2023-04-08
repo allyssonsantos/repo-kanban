8805ef70980b28e215aa914d8fa46b5431a01181
Allysson Santos <aallysson0@gmail.com>
2023-04-08 16:41:42 -0300

    feat: update board with new or deleted branches from origin

--- src/features/BoardFeature/hooks/useBoard.ts
+++ src/features/BoardFeature/hooks/useBoard.ts
@@ -1,5 +1,5 @@
+import { useEffect, useState } from 'react';
 import { useLocalStorage } from '@/hooks';
-import { useEffect } from 'react';
 import type { Board, Column } from '../model';
 import { boardService } from '../services';
 
@@ -14,20 +14,38 @@ type UpdateBranchStatus = {
  * In case of some column is removed, it will get all branches from that column
  * and put in the first column.
  *
- * It still needs to update the localStorage if some property of the available
+ * TODO: It still needs to update the localStorage if some property of the available
  * columns changes.
  *
- * @param currentBoard - current saved board
+ * @param savedBoard - current saved board
  * @param availableColumns - available columns
  * @returns A new updated board
  */
-function updateSavedBoard(
-  currentBoard: Board,
-  availableColumns: Omit<Column, 'items'>[]
-) {
-  if (currentBoard.length !== availableColumns.length) {
+function updateSavedBoard({
+  savedBoard,
+  availableColumns,
+  branches,
+}: {
+  savedBoard: Board;
+  availableColumns: Omit<Column, 'items'>[];
+  branches: string[];
+}) {
+  /**
+   * if number of columns of the current board is different from the available
+   * columns, it means that the saved board has less or more columns. The board
+   * service could have deleted or added columns.
+   * So, we need to remove or add the new columns, if some columns is deleted
+   * we need to move all the branches that were in that column to the first
+   * available column.
+   */
+  if (savedBoard.length !== availableColumns.length) {
+    /**
+     * we loop trough the available columns and if it already exists in the
+     * current board we just keep it as it is, if it's a new one we created it
+     * with empty items.
+     */
     const boardWithNewColumns = availableColumns.map((availableColumn) => {
-      const alreadyExists = currentBoard.find(
+      const alreadyExists = savedBoard.find(
         (current) => current.id === availableColumn.id
       );
       return {
@@ -36,16 +54,20 @@ function updateSavedBoard(
       };
     });
 
-    const orphanColumns = currentBoard.filter(
+    // here we get the deleted columns (if it exists)
+    const orphanColumns = savedBoard.filter(
       (currentColumn) =>
         !boardWithNewColumns.some(
           (newColumn) => newColumn.id === currentColumn.id
         )
     );
+
+    // here we get only the names of the saved branches from the deleted column
     const orphanBranches = orphanColumns
       .map((orphanColumn) => orphanColumn.items)
       .flat();
 
+    // put all the branches from the deleted column in the first one
     boardWithNewColumns[0].items = [
       ...boardWithNewColumns[0].items,
       ...orphanBranches,
@@ -54,19 +76,60 @@ function updateSavedBoard(
     return boardWithNewColumns;
   }
 
-  return currentBoard;
+  /**
+   * now we need to check if any new branch was added and add it to the first
+   * available column
+   */
+
+  // here we get saved branches from each columns
+  const savedBranches = savedBoard.reduce<string[]>(
+    (savedBranches, currentColumn) => {
+      savedBranches = [...savedBranches, ...currentColumn.items];
+      return savedBranches;
+    },
+    []
+  );
+
+  // now we will check if the branches from the API stays the same or if we get
+  // any new branch or if some was deleted.
+  const newBranches = branches.filter(
+    (branch) => !savedBranches.includes(branch)
+  );
+  const deletedBranches = savedBranches.filter(
+    (savedBranch) => !branches.includes(savedBranch)
+  );
+
+  if (newBranches.length || deletedBranches.length) {
+    const boardWithNewBranchesOnIt = savedBoard.map((column, index) => {
+      if (index === 0 && newBranches.length) {
+        column.items = [...column.items, ...newBranches];
+      }
+
+      const itemsWithoutDeletedBranch = column.items.filter(
+        (branch) => !deletedBranches.includes(branch)
+      );
+
+      return {
+        ...column,
+        items: itemsWithoutDeletedBranch,
+      };
+    });
+
+    return boardWithNewBranchesOnIt;
+  }
+
+  return savedBoard;
 }
 
 export function useBoard({
-  boardId,
   repoName,
   branches,
+  availableColumns,
 }: {
-  boardId: number;
   repoName: string;
   branches: string[];
+  availableColumns: Omit<Column, 'items'>[];
 }) {
-  const availableColumns = boardService.getBoardColumns(boardId).columns;
   const initialBoard = availableColumns.map((column, index) => ({
     id: column.id,
     key: column.key,
@@ -74,30 +137,38 @@ export function useBoard({
     items: index === 0 ? branches : [],
   }));
 
-  const [currentBoard, setCurrentBoard] = useLocalStorage<Board>(
+  const [savedBoard, setSavedBoard] = useLocalStorage<Board>(
     repoName,
     initialBoard
   );
 
+  const [updatedBoard, setUpdatedBoard] = useState<Board | null>(null);
+
   useEffect(() => {
-    if (currentBoard) {
-      setCurrentBoard(updateSavedBoard(currentBoard, availableColumns));
+    if (savedBoard) {
+      const newBoard = updateSavedBoard({
+        savedBoard,
+        availableColumns,
+        branches,
+      });
+
+      setUpdatedBoard(newBoard);
     }
-  }, [availableColumns, currentBoard, setCurrentBoard]);
+  }, [availableColumns, branches, savedBoard]);
 
   const updateBranchStatus = ({ branch, columnKey }: UpdateBranchStatus) => {
-    if (currentBoard) {
+    if (updatedBoard) {
       const newBoard = boardService.changeBoardBranchStatus({
-        board: currentBoard as Board,
+        board: updatedBoard,
         branch,
         columnKey,
       });
-      setCurrentBoard(newBoard);
+      setSavedBoard(newBoard);
     }
   };
 
   return {
-    board: currentBoard,
+    board: updatedBoard || savedBoard || initialBoard,
     availableColumns,
     updateBranchStatus,
   };
--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,14 +1,11 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 
 export const useLocalStorage = <T,>(key: string, initialValue: T) => {
   const [storedValue, setStoredValue] = useState<T | undefined>();
 
-  const setValue = useCallback(
-    (value: T) => {
-      window.localStorage.setItem(key, JSON.stringify(value));
-    },
-    [key]
-  );
+  const setValue = (value: T) => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
 
   useEffect(() => {
     const value = window.localStorage.getItem(key);
@@ -24,13 +21,15 @@ export const useLocalStorage = <T,>(key: string, initialValue: T) => {
     } else {
       setStoredValue(initialValue);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     if (storedValue) {
       setValue(storedValue);
     }
-  }, [setValue, storedValue]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [storedValue]);
 
   return [storedValue as T, setStoredValue] as const;
 };
